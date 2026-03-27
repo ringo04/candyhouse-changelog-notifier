eb5dbe1e568152abce0be7b5144a33d9ac94c8ea
Gota <73236379+ringo04@users.noreply.github.com>
2026-03-27 15:03:28 +0900

    Update post_to_x.py

--- post_to_x.py
+++ post_to_x.py
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     if last_sha and latest_sha:
         diff_text = get_file_diff(last_sha, latest_sha)
         # post_to_x(diff_text)
-        print("デバッグ開始！！！！！！！")
-        print(diff_text)
+        print("テストメッセージ", flush=True)
+        print(diff_text, flush=True)
